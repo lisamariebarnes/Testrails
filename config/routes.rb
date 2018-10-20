5a73b31cb055d8ed6c6cc02233599c7dd24330cc
lisamariebarnes <lisabarnes09@gmail.com>
2018-10-20 00:24:22 -0400

    Commit Message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about' => 'home#about'
 
+  get 'blog' => 'home#blog'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
